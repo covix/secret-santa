ac18cf7c4a186110e353e203c6521085c8784c5d
Luca Coviello <luca.coviello@gmail.com>
2019-11-25 11:19:16 +0100

    Formatted code and tested with Python3

--- secret_santa.py
+++ secret_santa.py
@@ -1,17 +1,19 @@
-import yaml
-# sudo pip install pyyaml
-import re
-import random
-import smtplib
 import datetime
-import pytz
-import time
-import socket
-import sys
 import getopt
 import os
+import random
 
-help_message = '''
+# sudo pip install pyyaml
+import re
+import smtplib
+import socket
+import sys
+import time
+
+import pytz
+import yaml
+
+help_message = """
 To use, fill out config.yml with your own participants. You can also specify 
 DONT-PAIR so that people don't get assigned their significant other.
 
@@ -19,19 +21,19 @@ You'll also need to specify your mail server settings. An example is provided
 for routing mail through gmail.
 
 For more information, see README.
-'''
+"""
 
 REQRD = (
-    'SMTP_SERVER', 
-    'SMTP_PORT', 
-    'USERNAME', 
-    'PASSWORD', 
-    'TIMEZONE', 
-    'PARTICIPANTS', 
-    'DONT-PAIR', 
-    'FROM', 
-    'SUBJECT', 
-    'MESSAGE',
+    "SMTP_SERVER",
+    "SMTP_PORT",
+    "USERNAME",
+    "PASSWORD",
+    "TIMEZONE",
+    "PARTICIPANTS",
+    "DONT-PAIR",
+    "FROM",
+    "SUBJECT",
+    "MESSAGE",
 )
 
 HEADER = """Date: {date}
@@ -43,46 +45,51 @@ Subject: {subject}
         
 """
 
-CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config.yml')
+CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yml")
+
 
 class Person:
     def __init__(self, name, email, invalid_matches):
         self.name = name
         self.email = email
         self.invalid_matches = invalid_matches
-    
+
     def __str__(self):
         return "%s <%s>" % (self.name, self.email)
 
+
 class Pair:
-    def __init__(self, giver, reciever):
+    def __init__(self, giver, receiver):
         self.giver = giver
-        self.reciever = reciever
-    
+        self.receiver = receiver
+
     def __str__(self):
-        return "%s ---> %s" % (self.giver.name, self.reciever.name)
+        return "%s ---> %s" % (self.giver.name, self.receiver.name)
+
 
 def parse_yaml(yaml_path=CONFIG_PATH):
-    return yaml.load(open(yaml_path))    
+    return yaml.load(open(yaml_path))
 
-def choose_reciever(giver, recievers):
-    choice = random.choice(recievers)
+
+def choose_receiver(giver, receivers):
+    choice = random.choice(receivers)
     if choice.name in giver.invalid_matches or giver.name == choice.name:
-        if len(recievers) is 1:
-            raise Exception('Only one reciever left, try again')
-        return choose_reciever(giver, recievers)
+        if len(receivers) is 1:
+            raise Exception("Only one receiver left, try again")
+        return choose_receiver(giver, receivers)
     else:
         return choice
 
+
 def create_pairs(g, r):
     givers = g[:]
-    recievers = r[:]
+    receivers = r[:]
     pairs = []
     for giver in givers:
         try:
-            reciever = choose_reciever(giver, recievers)
-            recievers.remove(reciever)
-            pairs.append(Pair(giver, reciever))
+            receiver = choose_receiver(giver, receivers)
+            receivers.remove(receiver)
+            pairs.append(Pair(giver, receiver))
         except:
             return create_pairs(g, r)
     return pairs
@@ -99,9 +106,9 @@ def main(argv=None):
     try:
         try:
             opts, args = getopt.getopt(argv[1:], "shc", ["send", "help"])
-        except getopt.error, msg:
+        except getopt.error as msg:
             raise Usage(msg)
-    
+
         # option processing
         send = False
         for option, value in opts:
@@ -109,25 +116,26 @@ def main(argv=None):
                 send = True
             if option in ("-h", "--help"):
                 raise Usage(help_message)
-                
+
         config = parse_yaml()
         for key in REQRD:
             if key not in config.keys():
                 raise Exception(
-                    'Required parameter %s not in yaml config file!' % (key,))
+                    "Required parameter %s not in yaml config file!" % (key,)
+                )
 
-        participants = config['PARTICIPANTS']
-        dont_pair = config['DONT-PAIR']
+        participants = config["PARTICIPANTS"]
+        dont_pair = config["DONT-PAIR"]
         if len(participants) < 2:
-            raise Exception('Not enough participants specified.')
-        
+            raise Exception("Not enough participants specified.")
+
         givers = []
         for person in participants:
-            name, email = re.match(r'([^<]*)<([^>]*)>', person).groups()
+            name, email = re.match(r"([^<]*)<([^>]*)>", person).groups()
             name = name.strip()
             invalid_matches = []
             for pair in dont_pair:
-                names = [n.strip() for n in pair.split(',')]
+                names = [n.strip() for n in pair.split(",")]
                 if name in names:
                     # is part of this pair
                     for member in names:
@@ -135,11 +143,12 @@ def main(argv=None):
                             invalid_matches.append(member)
             person = Person(name, email, invalid_matches)
             givers.append(person)
-        
-        recievers = givers[:]
-        pairs = create_pairs(givers, recievers)
+
+        receivers = givers[:]
+        pairs = create_pairs(givers, receivers)
         if not send:
-            print """
+            print(
+                """
 Test pairings:
                 
 %s
@@ -149,39 +158,48 @@ call with the --send argument:
 
     $ python secret_santa.py --send
             
-            """ % ("\n".join([str(p) for p in pairs]))
-        
+            """
+                % ("\n".join([str(p) for p in pairs]))
+            )
+
         if send:
-            server = smtplib.SMTP(config['SMTP_SERVER'], config['SMTP_PORT'])
-            server.starttls()
-            server.login(config['USERNAME'], config['PASSWORD'])
+            server = smtplib.SMTP_SSL(config["SMTP_SERVER"], config["SMTP_PORT"])
+            # server.starttls()
+            server.login(config["USERNAME"], config["PASSWORD"])
         for pair in pairs:
-            zone = pytz.timezone(config['TIMEZONE'])
+            zone = pytz.timezone(config["TIMEZONE"])
             now = zone.localize(datetime.datetime.now())
-            date = now.strftime('%a, %d %b %Y %T %Z') # Sun, 21 Dec 2008 06:25:23 +0000
-            message_id = '<%s@%s>' % (str(time.time())+str(random.random()), socket.gethostname())
-            frm = config['FROM']
+            date = now.strftime("%a, %d %b %Y %T %Z")  # Sun, 21 Dec 2008 06:25:23 +0000
+            message_id = "<%s@%s>" % (
+                str(time.time()) + str(random.random()),
+                socket.gethostname(),
+            )
+            frm = config["FROM"]
             to = pair.giver.email
-            subject = config['SUBJECT'].format(santa=pair.giver.name, santee=pair.reciever.name)
-            body = (HEADER+config['MESSAGE']).format(
-                date=date, 
-                message_id=message_id, 
-                frm=frm, 
-                to=to, 
+            subject = config["SUBJECT"].format(
+                santa=pair.giver.name, santee=pair.receiver.name
+            )
+            body = (HEADER + config["MESSAGE"]).format(
+                date=date,
+                message_id=message_id,
+                frm=frm,
+                to=to,
                 subject=subject,
                 santa=pair.giver.name,
-                santee=pair.reciever.name,
+                santee=pair.receiver.name,
             )
             if send:
-                result = server.sendmail(frm, [to], body)
-                print "Emailed %s <%s>" % (pair.giver.name, to)
+                server.sendmail(frm, [to], body)
+                print("Emailed %s <%s>" % (pair.giver.name, to))
 
         if send:
             server.quit()
-        
-    except Usage, err:
-        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-        print >> sys.stderr, "\t for help use --help"
+
+    except Usage as err:
+        # print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print(sys.argv[0].split("/")[-1] + ": " + str(err.msg))
+        # print >> sys.stderr, "\t for help use --help"
+        print("\t for help use --help")
         return 2
 
 
